719874291edd63d6d41e319eedbc0c07422bc531
Fatma-Eraslan <fatoseraslan275@gmail.com>
2023-09-10 18:02:44 +0300

    CQRS and Mediatr Implementation

---
+++ Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ApplicationServiceRegistration//program.cs de yapmak yerine bu şekilde clean kod
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());//mediatr git bütün assemblyleri tara oarada commendleri bul onların handlerlarını bul, birbiriyle eşleştir listele koy, send yaparsam onun handlerını çalıştır.
+            });
+            return services;    
+        }
+    }
+}
---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class BaseController:ControllerBase
+    {
+        private IMediator? _mediator;
+        //eğer daha önce mediator enjekte edilmiş ise onu(_mediator) döndür, değilse verdiğimi http yapısını döndür.
+        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        
+    }
+}

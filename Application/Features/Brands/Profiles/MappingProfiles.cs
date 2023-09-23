a802410132941634a12232fd38ce4da49f3025d1
Fatma-Eraslan <fatoseraslan275@gmail.com>
2023-09-23 17:53:18 +0300

    update and delete Command CQRS Implementation

--- Application/Features/Brands/Profiles/MappingProfiles.cs
+++ Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Commands.Delete;
+using Application.Features.Brands.Commands.Update;
 using Application.Features.Brands.Queries.GetById;
 using Application.Features.Brands.Queries.GetList;
 using AutoMapper;
@@ -19,6 +21,13 @@ namespace Application.Features.Brands.Profiles
         {
             CreateMap<Brand, CreateBrandCommand>().ReverseMap();//reversemap yani karşılıklı map
             CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+
+            CreateMap<Brand, UpdateBrandCommand>().ReverseMap();
+            CreateMap<Brand, UpdatedBrandResponse>().ReverseMap();
+            
+            CreateMap<Brand, DeleteBrandCommand>().ReverseMap();
+            CreateMap<Brand, DeletedBrandResponse>().ReverseMap();
+
             CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
             CreateMap<Brand, GetByIdBrandResponse>().ReverseMap();
             CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap();

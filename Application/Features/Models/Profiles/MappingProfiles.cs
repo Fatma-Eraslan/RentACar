12894e9abc6f11cec5537976824491ed83a3a10c
Fatma-Eraslan <fatoseraslan275@gmail.com>
2023-09-24 18:18:19 +0300

    Dynamic

--- Application/Features/Models/Profiles/MappingProfiles.cs
+++ Application/Features/Models/Profiles/MappingProfiles.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Features.Brands.Queries.GetList;
 using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using AutoMapper;
 using Core.Application.Responses;
 using Core.Persistence.Paging;
@@ -12,11 +13,24 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Models.Profiles;
 
-public class MappingProfiles:Profile
+public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
-        CreateMap<Model, GetListModelListItemDto>().ReverseMap();
-        CreateMap<Paginate<Model>,GetListResponse<GetListModelListItemDto>>().ReverseMap();
+        CreateMap<Model, GetListModelListItemDto>()
+            .ForMember(destinationMember: c => c.BrandName, memberOptions: opt => opt.MapFrom(c => c.Brand.Name))//name leri maple
+            .ForMember(destinationMember: c => c.FuelName, memberOptions: opt => opt.MapFrom(c => c.Fuel.Name))
+            .ForMember(destinationMember: c => c.TransmissionName, memberOptions: opt => opt.MapFrom(c => c.Transmission.Name))
+            .ReverseMap();
+
+        CreateMap<Model, GetListByDynamicModelListItemDto>()
+            .ForMember(destinationMember: c => c.BrandName, memberOptions: opt => opt.MapFrom(c => c.Brand.Name))
+            .ForMember(destinationMember: c => c.FuelName, memberOptions: opt => opt.MapFrom(c => c.Fuel.Name))
+            .ForMember(destinationMember: c => c.TransmissionName, memberOptions: opt => opt.MapFrom(c => c.Transmission.Name))
+            .ReverseMap();
+
+        CreateMap<Paginate<Model>, GetListResponse<GetListModelListItemDto>>().ReverseMap();
+
+        CreateMap<Paginate<Model>, GetListResponse<GetListByDynamicModelListItemDto>>().ReverseMap();
     }
 }
--- WebApi/Controllers/ModelsController.cs
+++ WebApi/Controllers/ModelsController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using Core.Application.Requests;
 using Core.Application.Responses;
+using Core.Persistence.Dynamic;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,10 +21,13 @@ namespace WebApi.Controllers
             return Ok(response);
         }
 
-
-
-
-
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] DynamicQuery? dynamicQuery = null)
+        {
+            GetListByDynamicModelQuery getListByDynamicModelQuery = new() { PageRequest = pageRequest, DynamicQuery = dynamicQuery };
+            GetListResponse<GetListByDynamicModelListItemDto> response = await Mediator.Send(getListByDynamicModelQuery);
+            return Ok(response);
+        }
 
     }
 }

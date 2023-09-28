7b4509308a2034ab6480645e6c58ae52010a3b64
Fatma-Eraslan <fatoseraslan275@gmail.com>
2023-09-28 22:27:00 +0300

    SubClass Type IoC implementasyonu

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Core.Application.Rules;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,18 +7,38 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application
+namespace Application;
+
+public static class ApplicationServiceRegistration//program.cs de yapmak yerine bu şekilde clean kod
 {
-    public static class ApplicationServiceRegistration//program.cs de yapmak yerine bu şekilde clean kod
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());//git assemblydeki mevcut çalışan her şeyin doğru seçim olduğunu
+
+        services.AddSubClassesOfType(Assembly.GetExecutingAssembly(),typeof(BaseBusinessRules)); //kullanımı
+
+        services.AddMediatR(configuration =>
         {
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());//git assemblydeki mevcut çalışan her şeyin doğru seçim olduğunu
-            services.AddMediatR(configuration =>
-            {
-                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());//mediatr git bütün assemblyleri tara oarada commendleri bul onların handlerlarını bul, birbiriyle eşleştir listele koy, send yaparsam onun handlerını çalıştır.
-            });
-            return services;    
-        }
+            configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());//mediatr git bütün assemblyleri tara oarada commendleri bul onların handlerlarını bul, birbiriyle eşleştir listele koy, send yaparsam onun handlerını çalıştır.
+        });
+        return services;    
     }
+
+    public static IServiceCollection AddSubClassesOfType(
+       this IServiceCollection services,
+       Assembly assembly,
+       Type type,
+       Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+   )
+    {//assembly içerisinde SubClass olarak verdiğimiz yani basebusisnessrule olanları bul, onları LifeCycle a ekle(ioc)
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+        foreach (var item in types)
+            if (addWithLifeCycle == null)
+                services.AddScoped(item);
+
+            else
+                addWithLifeCycle(services, type);
+        return services;
+    }
+
 }
